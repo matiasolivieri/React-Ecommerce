3b7247eac8da43cce40b5b02a5014f7a7baea306
Matias Olivieri <matiasolivieri34@gmail.com>
2022-08-18 21:58:49 -0300

    Firebase II | Clear Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.scss'
 import NavBar from './components/NavBar/NavBar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
 import Detail from './pages/Detail';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import CartPage from './pages/CartPage';
@@ -15,7 +14,6 @@ function App() {
                     <NavBar/>
                     <Routes>
                         <Route path="/" element={<Home/>}/>
-                        <Route path="/contacto" element={<Contact/>}/>
                         <Route path="/category/:categoryid" element={<ItemListContainer/>}/>
                         <Route path="/productos/:id" element={<Detail/>}/>
                         <Route path="/cart" element={<CartPage/>}/>
